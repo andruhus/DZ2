4bc77de69b7f2675e18b361877bfc6a1c6909a8b
andruhus <54791980+andruhus@users.noreply.github.com>
2019-11-04 00:42:04 +0200

    Add files via upload

---
+++ classwork1/class_h.h
@@ -0,0 +1,11 @@
+#pragma once
+void pr(float f, bool isNot0);
+void oper(float x, float a, float b, float c, bool isNot0);
+void reta();
+bool per(int a,int i, int j, int n, int m);
+void paint();
+void sevenoct();
+void ert();
+void cl2110();
+void cl281019();
+void man();
---
+++ classwork1/classwork.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <math.h>
+#include "class_h.h"
+using namespace std;
+
+
+
+int main()
+{
+	//reta();
+	//paint();
+	//sevenoct();
+	//ert();
+	//cl2110();
+	cl281019();
+	//man();
+	return 0;
+}
+
